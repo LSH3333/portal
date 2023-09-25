83f8a5a3b18b625033d0ec67d00fe0c5b8eece0f
LSH <dltpgustpgus@gmail.com>
2023-09-25 14:16:52 +0900

    작업중..

--- src/main/resources/static/js/slideObj.js
+++ src/main/resources/static/js/slideObj.js
@@ -1,17 +1,49 @@
 
-// function slide() {
-//     let mainBlock1 = document.getElementById('mainBlock1')
-//     let imgBlock1 = document.getElementById('imgBlock1')
-//     let mainBlock1Content = document.getElementById('mainBlock1Content')     
-// }
+let mainBlock1Content = document.getElementById('mainBlock1Content');
+let imgBlock1 = document.getElementById('imgBlock1')
+let mainBlock1ScrolledIn = false;
 
 
-// let mainBlockContent = document.getElementById('mainBlock1Content');
 
-// function slide() {
-//     console.log('slide!')
-//     mainBlockContent.classList.toggle('slide-right');
-// }
 
-// Call the slide function when you want to trigger the transition
-// slide();
+function activateSlideRight(elem) {
+    elem.classList.add('slide-right');
+}
+function deactivateSlideRight(elem) {
+    elem.classList.remove('slide-right');
+}
+
+function activateSlideLeft(elem) {
+    elem.classList.add('slide-left');
+}
+function deactivateSlideLeft(elem) {
+    elem.classList.remove('slide-left');
+}
+
+
+// Function to check if an element is in the viewport
+function isElementInViewport(elem) {
+    var rect = elem.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Add an event listener for the scroll event
+window.addEventListener('scroll', function () {
+    if (!mainBlock1ScrolledIn && isElementInViewport(document.getElementById('mainBlock1'))) {
+        mainBlock1ScrolledIn = true;
+        activateSlideRight(mainBlock1Content);
+        activateSlideLeft(imgBlock1);
+    }
+    if (mainBlock1ScrolledIn && !isElementInViewport(document.getElementById('mainBlock1'))) {
+        mainBlock1ScrolledIn = false;
+        deactivateSlideRight(mainBlock1Content);
+        deactivateSlideLeft(imgBlock1);
+    }
+
+
+});
