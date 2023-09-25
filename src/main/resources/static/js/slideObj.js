91f5ab0e291470779ad07777faf5976b6b61f40c
LSH <dltpgustpgus@gmail.com>
2023-09-25 15:32:36 +0900

    home 화면 스크롤 내리면 컨텐츠 slide 하도록 구현 완료

--- src/main/resources/static/js/slideObj.js
+++ src/main/resources/static/js/slideObj.js
@@ -1,8 +1,16 @@
 
+// mainBlock1
+let mainBlock1 = document.getElementById('mainBlock1');
 let mainBlock1Content = document.getElementById('mainBlock1Content');
 let imgBlock1 = document.getElementById('imgBlock1')
 let mainBlock1ScrolledIn = false;
-
+// mainBlock2
+let mainBlock2 = document.getElementById('mainBlock2');
+let mainBlock2Wrap = document.getElementById('mainBlock2Wrap')
+let mainBlock2ScrolledIn = false;
+// mainBlock3
+let mainBlock3 = document.getElementById('mainBlock3');
+let mainBlock3ScrolledIn = false;
 
 
 
@@ -24,26 +32,48 @@ function deactivateSlideLeft(elem) {
 // Function to check if an element is in the viewport
 function isElementInViewport(elem) {
     var rect = elem.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    // console.log('top = ' + rect.top)
+    // console.log('bottom = ' + rect.bottom)
+    // console.log('window.innerHeight = ' + window.innerHeight)
+
+    if (rect.bottom <= 0) {
+        return false;
+    }
+    if (rect.top <= (window.innerHeight || document.documentElement.clientHeight)) {
+        return true;
+    }
+    return false;
 }
 
 // Add an event listener for the scroll event
 window.addEventListener('scroll', function () {
-    if (!mainBlock1ScrolledIn && isElementInViewport(document.getElementById('mainBlock1'))) {
+    // mainBlock1
+    if (!mainBlock1ScrolledIn && isElementInViewport(mainBlock1)) {
         mainBlock1ScrolledIn = true;
         activateSlideRight(mainBlock1Content);
         activateSlideLeft(imgBlock1);
     }
-    if (mainBlock1ScrolledIn && !isElementInViewport(document.getElementById('mainBlock1'))) {
+    if (mainBlock1ScrolledIn && !isElementInViewport(mainBlock1)) {
         mainBlock1ScrolledIn = false;
         deactivateSlideRight(mainBlock1Content);
         deactivateSlideLeft(imgBlock1);
     }
-
-
+    // mainBlock2    
+    if (!mainBlock2ScrolledIn && isElementInViewport(mainBlock2)) {
+        mainBlock2ScrolledIn = true;
+        activateSlideLeft(mainBlock2Wrap);
+    }
+    if (mainBlock2ScrolledIn && !isElementInViewport(mainBlock2)) {
+        mainBlock2ScrolledIn = false;
+        deactivateSlideLeft(mainBlock2Wrap);
+    }
+    // mainBlock3
+    if (!mainBlock3ScrolledIn && isElementInViewport(mainBlock3)) {
+        mainBlock3ScrolledIn = true;
+        activateSlideLeft(mainBlock3)
+    }
+    if (mainBlock3ScrolledIn && !isElementInViewport(mainBlock3)) {
+        mainBlock3ScrolledIn = false;
+        deactivateSlideLeft(mainBlock3)
+    }
 });
