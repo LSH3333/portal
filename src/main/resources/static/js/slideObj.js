2b24f8182d68607442757d89ede8acec900ed685
LSH <dltpgustpgus@gmail.com>
2023-09-25 13:22:31 +0900

    작업중..

---
+++ src/main/resources/static/js/slideObj.js
@@ -0,0 +1,17 @@
+
+// function slide() {
+//     let mainBlock1 = document.getElementById('mainBlock1')
+//     let imgBlock1 = document.getElementById('imgBlock1')
+//     let mainBlock1Content = document.getElementById('mainBlock1Content')     
+// }
+
+
+// let mainBlockContent = document.getElementById('mainBlock1Content');
+
+// function slide() {
+//     console.log('slide!')
+//     mainBlockContent.classList.toggle('slide-right');
+// }
+
+// Call the slide function when you want to trigger the transition
+// slide();
