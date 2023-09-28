02bd11345a916c0761883ec6940be6ddd27e67eb
LSH <dltpgustpgus@gmail.com>
2023-09-28 21:00:10 +0900

    manygames 하위 목록 구현 완료

--- src/main/java/com/portal/portal/controller/HomeController.java
+++ src/main/java/com/portal/portal/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.portal.portal.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,33 +10,62 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("headerTextBoxText", "LSH's Portal");
         return "index";
     }
 
     @GetMapping("/portfolio")
-    public String portfolio() {
+    public String portfolio(Model model) {
+        model.addAttribute("headerTextBoxText", "PORTFOLIO");
         return "portfolio";
     }
 
     @GetMapping("/aboutme")
-    public String aboutme() {
+    public String aboutme(Model model) {
+        model.addAttribute("headerTextBoxText", "ABOUT ME");
         return "aboutme";
     }
 
     @GetMapping("/galaga")
-    public String galaga() {
+    public String galaga(Model model) {
+        model.addAttribute("headerTextBoxText", "GALAGA");
         return "galaga";
     }
 
     @GetMapping("instaweb")
-    public String instaweb() {
+    public String instaweb(Model model) {
+        model.addAttribute("headerTextBoxText", "INSTAWEB");
         return "instaweb";
     }
 
     @GetMapping("manygames")
-    public String manygames() {
+    public String manygames(Model model) {
+        model.addAttribute("headerTextBoxText", "MANYGAMES");
         return "manygames";
     }
 
+    @GetMapping("angrybird")
+    public String angrybird(Model model) {
+        model.addAttribute("headerTextBoxText", "ANGRYBIRD");
+        return "angrybird";
+    }
+
+    @GetMapping("flappybird")
+    public String flappybird(Model model) {
+        model.addAttribute("headerTextBoxText", "FLAPPYBIRD");
+        return "flappybird";
+    }
+
+    @GetMapping("jumper")
+    public String jumper(Model model) {
+        model.addAttribute("headerTextBoxText", "JUMPER");
+        return "jumper";
+    }
+
+    @GetMapping("nightmare")
+    public String nightmare(Model model) {
+        model.addAttribute("headerTextBoxText", "NIGHTMARE");
+        return "nightmare";
+    }
 }
